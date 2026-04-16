be3ba9d20f5774b79a796ecaa4d9d7db6a4ba967
ALP <a.b.lindgard@gmail.com>
2026-04-16 11:50:31 +0200

    added comment to the List in Storage.cs file

--- Classes/Storage.cs
+++ Classes/Storage.cs
@@ -5,7 +5,6 @@ namespace be_m2_generics.Classes;
 
 public class Storage<T>(StorageTypes storageTypes) : IStorageService<T>
 {
-    private List<T> _storage = new();
-
+    private List<T> _storage = new(); //? useful or pointless?
 
 }
