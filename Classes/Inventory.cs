a2dd679b727b2892777fbbfbc8b29614c7da6fb0
ALP <a.b.lindgard@gmail.com>
2026-04-16 10:15:21 +0200

    renamed base class from inventory.cs to storage.cs

--- Classes/Inventory.cs
+++
@@ -1,6 +0,0 @@
-namespace be_m2_generics.Classes;
-
-public class Inventory<T>
-{
-
-}
---
+++ Classes/Storage.cs
@@ -0,0 +1,8 @@
+using be_m2_generics.Interfaces;
+
+namespace be_m2_generics.Classes;
+
+public class Storage<T> : IStorageService
+{
+
+}
