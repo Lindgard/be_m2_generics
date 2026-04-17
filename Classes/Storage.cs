f1daa8d051fc577cbffe78222bd8956391fb4423
ALP <a.b.lindgard@gmail.com>
2026-04-17 12:27:33 +0200

    got the last method from AI

--- Classes/Storage.cs
+++ Classes/Storage.cs
@@ -3,7 +3,8 @@ namespace be_m2_generics.Classes;
 
 public class Storage<T>(StorageTypes storageTypes)
 {
-    private List<T> items = new(); //? useful or pointless?
+    private List<T> items = new();
+    private StorageTypes storageType = storageTypes;
 
     //TODO start AddItem method
     public void AddItem(T item)
@@ -19,8 +20,12 @@ public class Storage<T>(StorageTypes storageTypes)
 
     public int CountItems()
     {
-        //* no clue
-    };
+        return items.Count;
+    }
 
     //* RemoveItem method 
+    public void RemoveItem(T item)
+    {
+        items.Remove(item);
+    }
 }
