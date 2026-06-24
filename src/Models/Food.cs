d76fa632c66047ac78492597c6a57a24761679c7
ALP <a.b.lindgard@gmail.com>
2026-06-24 10:43:05 +0200

    Changed Weight from an integer to a double to allow for more precise weights

--- src/Models/Food.cs
+++ src/Models/Food.cs
@@ -3,7 +3,7 @@ using be_m2_generics.Classes;
 public class Food
 {
     public string Name { get; set; } = "";
-    public int Weight { get; set; }
+    public double Weight { get; set; }
     public string Category { get; set; } = "";
     public StorageTypes FoodCategory { get; set; }
 
